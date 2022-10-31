12cef133275e3ea34bf6d35d85b53fcac0ebf033
lordz-md <Traian.Ciobanu@endava.com>
2022-10-31 12:22:06 +0200

    Create stopec2.py

---
+++ stopec2.py
@@ -0,0 +1,13 @@
+import boto3
+import os
+
+def lambda_handler(event, context):
+    region = os.environ.get("REGION").strip()
+    ec2 = boto3.client('ec2', region_name=region)
+
+    ec2_instances = []
+    for reservation in ec2.describe_instances(Filters=[{'Name':'tag:Schedule','Values':['workinghours']}])['Reservations']:
+        for instance in reservation['Instances']:
+            ec2_instances.append(instance['InstanceId'])
+            ec2.stop_instances(InstanceIds=ec2_instances)
+            print('Stopped your instances: ' + str(ec2_instances))
